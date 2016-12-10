2bc1f3c2dfb281564cdefc8b2d6daf6ac7271509
Matt Daubney <matt@daubers.co.uk>
2016-12-10 14:50:35 +0000

    Add a climbable to cross the blender and sort out the camera angles

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, Camera.main.transform.position.z);
+    }
+}
