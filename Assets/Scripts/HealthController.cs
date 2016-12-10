d87056b887b81813a25c7cd162dc2797091eb624
Matt Daubney <matt@daubers.co.uk>
2016-12-10 18:03:01 +0000

    Added health gauge

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,16 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HealthController : MonoBehaviour {
 
     public int totalHealth = 1;
+    public GameObject heartFull;
+    public GameObject heartEmpty;
 
+    private bool isPlayer;
     private int currentHealth;
+    
+    private GameObject UICanvas;
+    private List<GameObject> heartGauge;
 
 	// Use this for initialization
 	void Start () {
-
+        
         currentHealth = totalHealth;
+
+        if (gameObject.tag == "Player")
+        {
+            isPlayer = true;
+            
+        }
+        else
+            isPlayer = false;
+
+        if (isPlayer)
+        {
+            heartGauge = new List<GameObject>();
+            UICanvas = GameObject.Find("UI");
+            updateHealthUI();
+        }
 
 	}
 	
@@ -32,6 +54,26 @@ public class HealthController : MonoBehaviour {
     {
         totalHealth = totalHealth + extraHealth;
         currentHealth = totalHealth;
+    }
+
+    private void updateHealthUI()
+    {
+        for (int i = 0; i < totalHealth; i++)
+        {
+            GameObject heartToUse;
+            Debug.Log(totalHealth-currentHealth);
+            if (i < (totalHealth - currentHealth))
+                heartToUse = heartEmpty;
+            else
+                heartToUse = heartFull;
+
+            if (i>=heartGauge.Count)
+            {
+                GameObject child = Instantiate(heartToUse);
+                child.transform.position = (heartToUse.GetComponent<RectTransform>().position) + (new Vector3(heartToUse.GetComponent<RectTransform>().sizeDelta.x*i, 0, 0));
+                child.transform.SetParent(UICanvas.transform, false);
+            }
+        }
     }
 
 	// Update is called once per frame
